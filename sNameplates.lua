e29acbc3bce16bd14d6d5c41f040524d6f583d80
Share <shaana@student.ethz.ch>
2012-11-09 22:45:46 +0100

    health bar

--- sNameplates.lua
+++ sNameplates.lua
@@ -6,18 +6,19 @@ local LibNameplate = LibStub("LibNameplate-1.0", true)
 
 
 
+
 function sNameplates:OnInizialize()
 
 end
 
 function sNameplates:OnEnable()
-	LibNameplate.RegisterCallback(self, "LibNameplate_NewNameplate")
-	LibNameplate.RegisterCallback(self, "LibNameplate_RecycleNameplate")
-	LibNameplate.RegisterCallback(self, "LibNameplate_FoundGUID")
+	--LibNameplate.RegisterCallback(self, "LibNameplate_NewNameplate")
+	--LibNameplate.RegisterCallback(self, "LibNameplate_RecycleNameplate")
+	--LibNameplate.RegisterCallback(self, "LibNameplate_FoundGUID")
 	LibNameplate.RegisterCallback(self, "LibNameplate_TargetNameplate")
 	--LibNameplate.RegisterCallback(self, "LibNameplate_MouseoverNameplate")
 	--LibNameplate.RegisterCallback(self, "LibNameplate_CombatChange")
-	LibNameplate.RegisterCallback(self, "LibNameplate_HealthChange")
+	--LibNameplate.RegisterCallback(self, "LibNameplate_HealthChange")
 	--LibNameplate.RegisterCallback(self, "LibNameplate_ThreatChange")
 
 end
@@ -26,12 +27,15 @@ function sNameplates:OnDisable()
 
 end
 
+share_plate = nil
+
 function sNameplates:LibNameplate_TargetNameplate(_, plate)
 	local plateName = LibNameplate:GetName(plate)
-	local health_frame = LibNameplate:GetHealthBar(plate)
+	local parent_frame = LibNameplate:GetHealthBar(plate):GetParent()
 	print(plateName)
-	print(health_frame)
-
+	print(parent_frame)
+	share_plate = plate
+	sNameplates:PrepareFrames(plate)
 end
 
 function sNameplates:test()
@@ -40,3 +44,29 @@ function sNameplates:test()
 	end
 
 end
+
+
+function sNameplates:PrepareFrames(plate)
+	plate.frame = {}
+	plate.parent = LibNameplate:GetHealthBar(plate):GetParent()
+	
+end
+
+local function Prepare_HealthBar(plate)
+	frame = plate.frame
+	
+	frame.health_bar = CreateFrame("StatusBar", nil, plate.parent)
+	frame.health_bar:SetStatusBarTexture("Interface\\AddOns\\sNameplates\\media\\bar")
+
+	frame.health_bar:ClearAllPoints()
+	--frame.health:SetSize(sizes.width-2, sizes.height-2)
+	frame.health_bar:SetPoint('BOTTOMLEFT', 20, 100)
+	
+	
+end
+
+local function Prepare_CastBar(plate)
+
+end
+
+
